21113fa421056283dfdd9de3b4fa8b461cf3fa61
Brandon Hernandez <brandonkhademi@gmail.com>
2016-10-29 14:15:28 -0500

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name constructionProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the constructionProjectApp
+ */
+angular.module('constructionProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name constructionProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the constructionProjectApp
+ */
+angular.module('constructionProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
